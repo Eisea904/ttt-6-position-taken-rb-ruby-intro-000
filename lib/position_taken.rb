aabe551eb20c268a6f8ce315f04f6e910e78e76d
Alex Eisenhauer <eisea904@newschool.edu>
2020-03-03 04:10:15 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,12 +1,8 @@
 # code your #position_taken? method here!
 def position_taken? (board, index)
-<<<<<<< HEAD
  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
   end
-=======
-  board[index] == ("  " || "" || "nil")
->>>>>>> 9aab9385e32d329cf2e9368e6b4ed316323d0f06
 end
